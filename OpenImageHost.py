b6e7ac666b88290a4a403b3d264c0ac22bb741fa
zoutao98 <ezoutao@foxmail.com>
2022-11-22 22:59:59 +0800

    初始代码

--- OpenImageHost.py
+++ OpenImageHost.py
@@ -1,2 +1,44 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 
+class ImageHostConf():
+
+    def __init__(self) -> None:
+        pass
+
+    def readConf(self):
+        pass
+
+    def writeConf(self):
+        pass
+
+    def updateConf(self):
+        pass
+
+    def getConf(self, conf: str):
+        pass
+
+class OpenImageHost(QtWidgets.QWidget):
+    def __init__(self) -> None:
+        super().__init__()
+        self.setWindowTitle(self.tr('Open图床'))
+        self.sidelist=QtWidgets.QListWidget()
+        self.sideBar()
+        self.mainStack=QtWidgets.QStackedWidget()
+        self.mainView()
+
+        HBox=QtWidgets.QHBoxLayout()
+        HBox.addWidget(self.sidelist)
+        HBox.addWidget(self.mainStack)
+        self.setLayout(HBox)
+
+    def sideBar(self):
+        pass
+    
+    def mainView(self):
+        pass
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication()
+    openImageHost = OpenImageHost()
+    openImageHost.show()
+    app.exec_()
