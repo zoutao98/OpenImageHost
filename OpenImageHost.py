1e08ee271d1b0ce81f962e435b47fe8485c5c31e
zoutao98 <ezoutao@foxmail.com>
2022-11-23 09:06:50 +0800

    声明许可证

--- OpenImageHost.py
+++ OpenImageHost.py
@@ -1,4 +1,22 @@
-from PySide2 import QtCore, QtGui, QtWidgets
+'''
+   Copyright 2022 zoutao
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
+from PySide2 import QtCore, QtGui
+from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtWidgets import QListWidget, QStackedWidget
+from PySide2.QtWidgets import QHBoxLayout
 
 class ImageHostConf():
 
@@ -17,16 +35,16 @@ class ImageHostConf():
     def getConf(self, conf: str):
         pass
 
-class OpenImageHost(QtWidgets.QWidget):
+class OpenImageHost(QWidget):
     def __init__(self) -> None:
         super().__init__()
         self.setWindowTitle(self.tr('Open图床'))
-        self.sidelist=QtWidgets.QListWidget()
+        self.sidelist=QListWidget()
         self.sideBar()
-        self.mainStack=QtWidgets.QStackedWidget()
+        self.mainStack=QStackedWidget()
         self.mainView()
 
-        HBox=QtWidgets.QHBoxLayout()
+        HBox=QHBoxLayout()
         HBox.addWidget(self.sidelist)
         HBox.addWidget(self.mainStack)
         self.setLayout(HBox)
@@ -38,7 +56,7 @@ class OpenImageHost(QtWidgets.QWidget):
         pass
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication()
+    app = QApplication()
     openImageHost = OpenImageHost()
     openImageHost.show()
     app.exec_()
